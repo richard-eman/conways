228305049d41e2980fc55d8635117fa23e41bd8f
richard-eman <richardemanuel1205@gmail.com>
2021-03-06 13:13:49 +1300

    create createBoard function

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,12 @@
 function createBoard (size) {
-
+  const board = []
+  for (let i = 0; i < size; i++){
+    board[i] = []
+    for (let y = 0; y < size; y++){
+      board[i].push([])
+    }
+  }
+  return board
 }
 
 module.exports = createBoard
